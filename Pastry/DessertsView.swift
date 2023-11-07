f7d02326fb6fc072fac51095de4775a72315d96c
Erik Perez <erikperez@Eriks-MacBook-Pro.local>
2023-11-06 18:26:15 -0600

    Initial Commit

---
+++ Pastry/DessertsView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertsView.swift
+//  Pastry
+//
+//  Created by Erik Perez on 11/6/23.
+//
+
+import SwiftUI
+
+struct DessertsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    DessertsView()
+}
---
+++ Pastry/PastryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastryApp.swift
+//  Pastry
+//
+//  Created by Erik Perez on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PastryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DessertsView()
+        }
+    }
+}
